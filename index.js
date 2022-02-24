46cf936fa3bcef7d03c7a08557365436d0333896
Andres Olaya <acrista04766@universidadean.edu.co>
2022-02-24 09:45:41 -0500

    Second

---
+++ index.js
@@ -0,0 +1,18 @@
+const button1 = document.getElementById("button1");
+const modal =  document.querySelector('.modal');
+const modelClose = document.getElementById('modal__content--close');
+
+function removeClass (modal) {
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+};
+
+button1.addEventListener('click', () => {
+    removeClass(modal);
+})
+
+modelClose.addEventListener('click', () => {
+    modal.classList.remove('visible');
+    modal.classList.add('hidden');
+})
+
